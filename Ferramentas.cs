4ac8602c9b9eab85800e5bdb69905ece74f000a8
LUCASGOODPROGRAMMER <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2024-04-22 10:12:46 -0400

    Add files via upload

---
+++ Ferramentas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDiária
+{
+    public static class Ferramentas
+    {
+        public static TimeSpan CalcularDiária(double valorDiaria, DateTime DataEntrada, DateTime DataSaida)
+        {
+            /*
+            double valorHora = valorDiaria / 24;
+            double valorMinuto = valorDiaria / 1440;
+            double valorSegundo = valorDiaria / 86400;
+            */
+            if (DataSaida > DataEntrada)
+            {
+                TimeSpan Hora = DataSaida.Subtract(DataEntrada);
+                
+                return Hora;
+            }
+            else
+            {
+                TimeSpan Hora = DataEntrada.Subtract(DataSaida);
+               
+                return Hora;
+            }
+           
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using BibliotecaDiária;
+Console.ForegroundColor = ConsoleColor.White;   
+
+Console.Write("Informe a data de entrada: ");
+DateTime DataEntrada = Convert.ToDateTime(Console.ReadLine());
+
+Console.Write("Inforrme a data de saida: ");
+DateTime DataSaida = Convert.ToDateTime(Console.ReadLine());
+
+Console.Write("Informe o valor da diária:R$");
+double valorDiaria = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("diferença formato DateTime:" + Ferramentas.CalcularDiária(valorDiaria, DataEntrada, DataSaida));
+string data = Convert.ToString(Ferramentas.CalcularDiária(valorDiaria, DataEntrada, DataSaida).TotalDays);
+double valorReal = valorDiaria * Convert.ToDouble(data);
+
+Console.WriteLine("Diferença de dias quebrado:" + data);
+Console.WriteLine("O valor a ser pego pelo seu tempo de trabalho sera de:" + valorReal.ToString("C2"));
